12ad00b95f3d3c4947b1d9c54a276743268235e5
joshparkerj <superfakek@gmail.com>
2019-12-21 17:14:23 -0800

    add example code from magick.net manual

--- pixel-cursive-writing-gif-generator/Program.cs
+++ pixel-cursive-writing-gif-generator/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ImageMagick;
+using System;
 
 namespace pixel_cursive_writing_gif_generator
 {
@@ -7,6 +8,36 @@ namespace pixel_cursive_writing_gif_generator
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+        }
+
+        /// <summary>
+        /// Code copied from the Magick.NET manual here:
+        /// https://github.com/dlemstra/Magick.NET/blob/master/docs/CombiningImages.md
+        /// </summary>
+        static void CreateAnimatedGif()
+        {
+            using (MagickImageCollection collection = new MagickImageCollection())
+            {
+                // Add first image and set the animation delay (in 1/100th of a second) 
+                collection.Add("Snakeware.png");
+                collection[0].AnimationDelay = 100; // in this example delay is 1000ms/1sec
+
+                // Add second image, set the animation delay (in 1/100th of a second) and flip the image
+                collection.Add("Snakeware.png");
+                collection[1].AnimationDelay = 100; // in this example delay is 1000ms/1sec
+                collection[1].Flip();
+
+                // Optionally reduce colors
+                QuantizeSettings settings = new QuantizeSettings();
+                settings.Colors = 256;
+                collection.Quantize(settings);
+
+                // Optionally optimize the images (images should have the same size).
+                collection.Optimize();
+
+                // Save gif
+                collection.Write("Snakeware.Animated.gif");
+            }
         }
     }
 }
